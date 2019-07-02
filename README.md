bca1f2a2cbd5e4189241db8686388871d60cad31
Matthew Nicol <matthew@communityds.com.au>
2019-07-02 01:04:21 +0000

    Update structure before reuploding to pypi

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # php_whisperer
+*"I heard you help people with PHP problems?" "Truth is, I help PHP with people problems."*
+
+Convert PHP arrays to Python objects using read_php
+
+`from php_whisperer import read_php
+read_php('/tmp/a_php_file.py', variable='data')
+>>> {'My Php Array': ['You', 'get', 'the', 'point']}
+`
+
+Convert Python lists and dictionaries to PHP using generate_php
+
+`from php_whisperer import generate_php
+generate_php([1, 2, 3, 4])
+
+array(1, 2, 3, 4);`
+
--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-        name="php_whisperer",
-        version="1.3",
-        author="Matthew Nicol",
-        author_email="matthew.b.nicol@gmail.com",
-        description="Read and write php arrays with python",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url="https://github.com/matthewnicol/php_whisperer",
-        packages=setuptools.find_packages(),
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent"
-        ],
+    name="php_whisperer",
+    version="1.4",
+    author="Matthew Nicol",
+    author_email="matthew.b.nicol@gmail.com",
+    description="Read and write php arrays with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matthewnicol/php_whisperer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
 )
 
 
